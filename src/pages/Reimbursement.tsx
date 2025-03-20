3b8bd39afa9009cc06b4f6a7a98a12585f42deff
st-vignesh1 <vignesh@volopay.co>
2025-03-20 15:55:15 +0530

    Added floating button on the reimbursement page

--- src/pages/Reimbursement.tsx
+++ src/pages/Reimbursement.tsx
@@ -1,10 +1,10 @@
-import { View, Text, SafeAreaView, FlatList } from 'react-native';
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
 import React, { useState } from 'react';
 import Claims from '../components/core/Claims';
 import { claimsData } from '../constants/claimsData';
 import SearchBar from '../components/core/SearchBar';
 const Reimbursement = () => {
-    const [searchValue,setSearchValue]=useState('');
+    const [searchValue,setSearchValue] = useState('');
     const [claims, setClaims] = useState(() => {
         return claimsData?.length ? claimsData : [];
       });
@@ -23,8 +23,11 @@ const Reimbursement = () => {
       <View className="pl-4 pr-4 mb-8">
       <SearchBar value={searchValue} handleChange={handleSearchData}/>
       </View>
-      {claims?.length>0? <FlatList data={claims} renderItem={({item})=><Claims data={item} />} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
+      {claims?.length > 0 ? <FlatList data={claims} renderItem={({item})=><Claims data={item} />} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
         </View>
+        <TouchableOpacity className="absolute bottom-28 right-8 w-16 h-16 bg-blue-500 border border-blue-500 rounded-full items-center justify-center">
+         <Text className='text-2xl text-white font-semibold'>+</Text>
+        </TouchableOpacity>
     </SafeAreaView>
   );
 };
