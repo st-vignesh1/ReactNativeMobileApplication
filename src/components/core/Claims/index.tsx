50f2d34055a52775bac6dc32d264dd620c3b2279
st-vignesh1 <vignesh@volopay.co>
2025-03-20 16:33:13 +0530

    Implemented generating random color from the colorPallete

--- src/components/core/Claims/index.tsx
+++ src/components/core/Claims/index.tsx
@@ -4,12 +4,16 @@ import React from 'react';
 
 
 
-const Claims = ({data}) => {
-  return (
+const Claims = ({data,color}) => {
+    console.log(color)
+;  return (
    <SafeAreaView>
     <View className='w-full h-fit flex-row p-4 justify-between items-center'>
     <View className='flex-row items-center gap-4'>
-        <View className='w-10 h-10 rounded-full bg-black'></View>
+        <View style={{backgroundColor:color}} className=" w-12 h-12 rounded-full flex justify-center items-center "><Text className='text-white text-xl font-bold capitalize'>
+            {data.company[0]}
+            </Text>
+            </View>
         <View className='flex-col gap-1'>
             <Text className='capitalize font-bold'>{data.company}</Text>
             <Text className={`capitalize text-xs font-semibold ${data.status==="approved"?"text-green-600":"text-yellow-600"}`}>{data.status}</Text>
--- src/pages/Reimbursement.tsx
+++ src/pages/Reimbursement.tsx
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
 import React, { useState } from 'react';
 import Claims from '../components/core/Claims';
-import { claimsData } from '../constants/claimsData';
+import { claimsData, colorPallete } from '../constants/claimsData';
 import SearchBar from '../components/core/SearchBar';
 const Reimbursement = () => {
     const [searchValue,setSearchValue] = useState('');
@@ -15,6 +15,10 @@ const Reimbursement = () => {
             setClaims(claimsData.filter(claim=>claim.company.startsWith(value.toLowerCase())));
         }
     }
+    function randomColor(){
+        const index=Math.floor(Math.random()*colorPallete.length);
+         return `bg-[rgb(${colorPallete[index]})]`;
+    }
 
   return (
     <SafeAreaView>
@@ -23,7 +27,7 @@ const Reimbursement = () => {
       <View className="pl-4 pr-4 mb-8">
       <SearchBar value={searchValue} handleChange={handleSearchData}/>
       </View>
-      {claims?.length > 0 ? <FlatList data={claims} renderItem={({item})=><Claims data={item} />} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
+      {claims?.length > 0 ? <FlatList data={claims} renderItem={({item})=><Claims data={item} color={randomColor()}/>} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
         </View>
         <TouchableOpacity className="absolute bottom-28 right-8 w-16 h-16 bg-blue-500 border border-blue-500 rounded-full items-center justify-center">
          <Text className='text-2xl text-white font-semibold'>+</Text>
