5bc8e29a1933d3a917ab4ecc6d12f2f99172c959
st-vignesh1 <vignesh@volopay.co>
2025-03-20 15:20:12 +0530

    Implemented search functionality

--- src/pages/Reimbursement.tsx
+++ src/pages/Reimbursement.tsx
@@ -1,19 +1,29 @@
 import { View, Text, SafeAreaView, FlatList } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Claims from '../components/core/Claims';
 import { claimsData } from '../constants/claimsData';
 import SearchBar from '../components/core/SearchBar';
-
-
 const Reimbursement = () => {
+    const [searchValue,setSearchValue]=useState('');
+    const [claims, setClaims] = useState(() => {
+        return claimsData?.length ? claimsData : [];
+      });
+    function handleSearchData(value){
+        setSearchValue(value);
+        if(!value) {setClaims(claimsData);}
+        else{
+            setClaims(claimsData.filter(claim=>claim.company.startsWith(value.toLowerCase())));
+        }
+    }
+
   return (
     <SafeAreaView>
         <View className="w-full min-h-screen p-4">
       <Text className="mb-14 pl-4 text-2xl font-semibold">Reimbursement</Text>
       <View className="pl-4 pr-4 mb-8">
-      <SearchBar/>
+      <SearchBar value={searchValue} handleChange={handleSearchData}/>
       </View>
-      {claimsData?.length ? <FlatList data={claimsData} renderItem={({item})=><Claims data={item} />} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
+      {claims?.length>0? <FlatList data={claims} renderItem={({item})=><Claims data={item} />} className='w-full h-fit flex-col gap-4'/>:<Text>NO CLAIMS YET!</Text>}
         </View>
     </SafeAreaView>
   );
